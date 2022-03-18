b59d4d877deed8dbab67b99806bfcacaaf2fa489
Dominik Tanko <tankodominik@LAPTOP-8KFSOBOJ.localdomain>
2022-03-18 17:47:54 +0100

    Change l'emplacement des fonctions heuristique best-fit

--- heuristique_best-fit.jl
+++ heuristique_best-fit.jl
@@ -14,7 +14,52 @@ struct donnees1D
 	tab::Vector{objet1D} # Tableau des objets à insérer dans les bins
 end
 
-function trie_objet1D(l_obj1D::Vector{objet1D})
-    
+# Structure représentant un bin
+struct bin1D
+    taille::Int64
+    taille_disponible::Int64
+    tab::Vector{objet1D}
+end
+
+
+# Converti un objet 1D en un tuple (taille, nb)
+function objet1D_en_tuple(obj::objet1D)
+    return obj.taille,obj.nb; 
+end
+
+# Converti une liste d'objet en une liste de tuple
+function liste_objet_en_tuple(listeObjet::Vector{objet1D})
+
+    nouvelleListe::Vector{Tuple{Int64,Int64}}
+
+    for obj in listeObjet
+        nouveauTuple::Tuple{Int64,Int64} = objet1D_en_tuple(obj)
+        append!(nouvelleListe,nouveauTuple)
+    end
+
+    return nouvelleListe
+end
+
+function trie_objet1D(listeObjet::Vector{objet1D})
+
+    listeTuple::Vector{Tuple{Int64,Int64}} = liste_objet_en_tuple(listeObjet)
+
+    sort!(listeTuple,by = x -> x[1]);
+
+    return listeTuple
+end
+
+function heuristique_fit1D(listeObjet::Vector{objet1D})
+
+    listeTuple::Vector{Tuple{Int64,Int64}} = trie_objet1D(listeObjet)
+
+    listBin::Vector{bin1D} 
+
+	# TODO
+    for t in listeTuple
+        for b in listBin
+
+        end
+    end
 
 end
