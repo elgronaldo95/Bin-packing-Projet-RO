cdf1e40d26e91da7a1c7228a0a97226b065b442f
Dominik Tanko <tankodominik@LAPTOP-8KFSOBOJ.localdomain>
2022-03-20 12:22:49 +0100

    First non-optimized version

--- heuristique_best-fit.jl
+++ heuristique_best-fit.jl
@@ -15,7 +15,7 @@ end
 # Converti une liste d'objet en une liste de tuple
 function liste_objet_en_tuple(listeObjet::Vector{objet1D})
 
-    taille::Int64 = length(listeObjet)
+    taille = length(listeObjet)
 
     nouvelleListe = Vector{Tuple{Int64,Int64}}()
 
@@ -27,12 +27,12 @@ function liste_objet_en_tuple(listeObjet::Vector{objet1D})
     return nouvelleListe
 end
 
-# Trie une liste d'objet par ordre croissant de leur taille
+# Trie une liste d'objet par ordre decroissant de leur taille
 function trie_objet1D(listeObjet::Vector{objet1D})
     resListeObjet = Vector{objet1D}()
     listeTuple = liste_objet_en_tuple(listeObjet)
 
-    sort!(listeTuple,by = x -> x[1]);
+    sort!(listeTuple, by = x -> x[1], rev=true);
 
     for t in listeTuple
         newObj = objet1D(t[1], t[2])
@@ -63,18 +63,26 @@ function heuristique_fit1D(d1D::donnees1D)
     newBin = bin1D(tailleBin, tailleBin, [])
     listeBin = [newBin]
 
-	# TODO
     for obj in listeObjet
-        for b in listeBin
-            if b.taille_disponible >= obj.taille
-                append!(b.tab, obj)
-                b.taille_disponible = (b.taille_disponible - obj.taille)
+        for i in 1:length(listeBin)
+            if listeBin[i].taille_disponible < obj.taille
+                if length(listeBin) > i
+                    i = i+1
+                else
+                    newBin = bin1D(tailleBin, (tailleBin-obj.taille), [])
+                    append!(newBin.tab, [obj])
+                    append!(listeBin, [newBin])
+                    break
+                end
             else
-                newBin = bin1D(tailleBin, tailleBin, [])
+                append!(listeBin[i].tab, [obj])
+                listeBin[i].taille_disponible = (listeBin[i].taille_disponible - obj.taille)
+                break
             end
         end
     end
 
+    return listeBin
 end
 
 #= ************************* DEBUG AND TESTS ************************* =#
@@ -85,3 +93,22 @@ l_obj_trie = trie_objet1D(l_obj)
 l_obj_flat = flatten_liste_objet1D(l_obj_trie)
 
 l_obj_final = flatten_liste_objet1D(trie_objet1D([objet1D(5,3), objet1D(2,1), objet1D(3,3), objet1D(1,1)]))
+
+d1 = donnees1D(10, 4, [objet1D(5,3), objet1D(2,1), objet1D(3,3), objet1D(1,1)])
+
+myListeBin = heuristique_fit1D(d1)
+
+
+
+#=
+if b.taille_disponible >= obj.taille
+    append!(b.tab, [obj])
+    b.taille_disponible = (b.taille_disponible - obj.taille)
+    break
+else
+    newBin = bin1D(tailleBin, tailleBin, [])
+    append!(newBin.tab, [obj])
+    newBin.taille_disponible = (newBin.taille_disponible - obj.taille)
+    append!(listeBin, [newBin])
+end
+            =#
