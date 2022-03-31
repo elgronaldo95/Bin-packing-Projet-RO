5502e12a1913b52ee703e2fb9be9f33375cd6e66
Dominik Tanko <tankodominik@LAPTOP-8KFSOBOJ.localdomain>
2022-03-31 17:48:26 +0200

    Complete calcul des motifs

--- indirect_v2.jl
+++ indirect_v2.jl
@@ -1,12 +1,37 @@
 using JuMP, GLPK
 
-struct arbre_type
-    place_restant::Int64
-    taille_objet::Int64
-    fils::Vector{arbre_type}
+function motifs_calc(type_courant::Int, somme::Int, taille_bin::Int, motif_local::Vector{Int}, types::Vector{Int}, motifs::Vector{Vector{Int}})
+    ajout::Bool = false
+
+    for type in type_courant:size(types,1)
+        
+        nouveau_somme = somme + types[type]
+
+        if (nouveau_somme <= taille_bin)
+            ajout = true
+
+            push!(motif_local, type)
+
+            vcat(motifs, motifs_calc(type, nouveau_somme, taille_bin, deepcopy(motif_local), types, motifs))
+ 
+            pop!(motif_local)
+        end
+    end
+
+    if (!ajout)
+        push!(motifs, motif_local)
+    end
+    return motifs
+end
+
+
+function tout_motifs(taille_bin::Int64, types::Vector{Int64})
+
+    motif_local::Vector{Int} = []
+    motifs::Vector{Vector{Int}} = [[]]
+
+    return motifs_calc(1, 0, taille_bin, motif_local, types, motifs)
 
 end
 
-function parcours_arbre()
-
-end
+test = tout_motifs(10, [5,4,2])
